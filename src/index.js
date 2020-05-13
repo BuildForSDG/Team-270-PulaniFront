e2a3fda9efede5f6f28ee65c564ee559f69b2e0e
Martin Matovu <matovu.synergy@gmail.com>
2020-05-13 12:57:23 +0300

    Refactor the index.(html|js|css) serve the react app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import app from './app';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'proxy-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-const startApp = async () => {
-  const header = document.querySelector('[data-app-name]');
-  if (!header) return;
-
-  const programName = await app();
-  header.textContent = programName;
-};
-
-document.addEventListener('DOMContentLoaded', startApp);
+ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.register();
