ef616fcc51a24051e6600ffa0dc37ba1309d2376
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2020-05-22 17:51:35 -1200

    imported authProvider

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Admin } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-
+import authProvider from './AuthProvider';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider}/>;
+const App = () => <Admin authProvider={authProvider} dataProvider={dataProvider} />;
 
 export default App;
