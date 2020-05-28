2185609397e9b137531a66447404ddb8d76050aa
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2020-05-27 22:05:12 -1200

    Refactor the <App/> with new props

--- src/app.js
+++ src/app.js
@@ -1,8 +1,56 @@
-import React from 'react';
-import { Admin } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import authProvider from './AuthProvider';
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin authProvider={authProvider} dataProvider={dataProvider} />;
+import React, { useState, useEffect } from 'react';
+import { Admin, fetchUtils, Resource } from 'react-admin';
+
+import dataProvider from './dataProvider';
+import authProvider from './authProvider';
+import i18nProvider from './i18n';
+import { PulaniLayout } from './layout';
+import PulaniLoginPage from './login';
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  const fetchDataProvider = async () => {
+    const httpClient = (url, options) => {
+      if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+      }
+      const token = sessionStorage.getItem('token');
+      options.headers.set('Authorization', `Bearer ${token}`);
+      return fetchUtils.fetchJson(url, options);
+    };
+
+    let dataProviderInstance = await dataProvider(process.env.REACT_APP_HEROKU, httpClient);
+
+    setData(
+      // NOTE: dataProviderInstance can be a function
+      () => dataProviderInstance
+    );
+  };
+
+  useEffect(() => {
+    fetchDataProvider();
+  }, []);
+
+  if (!data) {
+    return (
+      <div className="loader-container">
+        <div className="loader">Loading...</div>
+      </div>
+    );
+  }
+  return (
+    <Admin
+      title=""
+      dataProvider={data}
+      authProvider={authProvider}
+      i18nProvider={i18nProvider}
+      layout={PulaniLayout}
+      loginPage={PulaniLoginPage}
+    >
+      <Resource name="users" />
+    </Admin>
+  );
+};
 
 export default App;
