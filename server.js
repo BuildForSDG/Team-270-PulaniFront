55dff81a49d6f6c86af9803e0776c2115e45260c
Martin Matovu <matovu.synergy@gmail.com>
2020-06-10 17:27:18 +0300

    Update the express file, to reduce redirects

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.static(publicPath));
 // ensure that index.html is served
 app.get('*', (req, res, next) => {
 	if (req.headers['x-forward-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
-		res.redirect('https://' + req.headers.host + req.url);
+		// res.redirect('https://' + req.headers.host + req.url);
 		res.sendFile(path.join(publicPath, 'index.html'));
 	} else {
 		next();
 	}
 });
 
-// start the server on given port (local env: port 3000)
+// start the server on given port (local env: port 1234)
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}!`);
 });
